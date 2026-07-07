905a6b18a03bbbfdd7cdfe93285bd9292677d59e
初行 <3352514932@qq.com>
2026-07-07 16:28:02 +0800

    add src/features/layout/components/AppLayout.vue

---
+++ src/features/layout/components/AppLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HeaderBar from "./HeaderBar.vue"
+</script>
+
+<template>
+  <div class="dashboard-container">
+    <HeaderBar />
+    <main class="dashboard-main">
+      <slot />
+    </main>
+  </div>
+</template>
