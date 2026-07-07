b0dc8cb7e7cb688c6682907da7977c7f213d7f3a
初行 <3352514932@qq.com>
2026-07-07 16:27:49 +0800

    add src/features/charts/components/SalesTrendChart.vue

---
+++ src/features/charts/components/SalesTrendChart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { useDashboardStore } from "@/features/dashboard/store"
+import BaseEChart from "./BaseEChart.vue"
+import { createSalesTrendOption } from "@/features/charts/config/sales-trend"
+
+const store = useDashboardStore()
+const option = computed(() => createSalesTrendOption(store.salesTrend))
+</script>
+
+<template>
+  <BaseEChart :option="option" />
+</template>
