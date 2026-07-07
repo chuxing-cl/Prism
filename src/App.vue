36ad9737d07c12191910436bb93f48092818a3d4
初行 <3352514932@qq.com>
2026-07-07 16:27:10 +0800

    add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { onMounted } from "vue"
+import { setupGlobalErrorHandler } from "@/core/error-handler"
+
+onMounted(() => {
+  setupGlobalErrorHandler()
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
