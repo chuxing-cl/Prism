6c4b4b035c70d5673928c86a9d2394656459b2d2
初行 <3352514932@qq.com>
2026-07-07 16:28:12 +0800

    add src/shared/composables/useAutoRefresh.ts

---
+++ src/shared/composables/useAutoRefresh.ts
@@ -0,0 +1,21 @@
+import { onBeforeUnmount, ref } from "vue"
+
+export function useAutoRefresh(callback: () => void, interval: number) {
+  const timer = ref<ReturnType<typeof setInterval> | null>(null)
+
+  function start() {
+    stop()
+    timer.value = setInterval(callback, interval)
+  }
+
+  function stop() {
+    if (timer.value) {
+      clearInterval(timer.value)
+      timer.value = null
+    }
+  }
+
+  onBeforeUnmount(() => stop())
+
+  return { start, stop }
+}
