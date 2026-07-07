fdfa82b441d9dfc022670e5fae6f1e609e308c79
初行 <3352514932@qq.com>
2026-07-07 16:28:08 +0800

    add src/shared/components/DataPanel.vue

---
+++ src/shared/components/DataPanel.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="chart-card">
+    <div class="chart-title">{{ title }}</div>
+    <div class="chart-body">
+      <slot />
+    </div>
+  </div>
+</template>
