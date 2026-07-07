6fc4e60d94a1917d3fcb1c7ad59f5879f4aac18c
初行 <3352514932@qq.com>
2026-07-07 16:27:19 +0800

    add src/core/logger/types.ts

---
+++ src/core/logger/types.ts
@@ -0,0 +1,19 @@
+export enum LogLevel {
+  DEBUG = 0,
+  INFO = 1,
+  WARN = 2,
+  ERROR = 3,
+}
+
+export interface LogEntry {
+  timestamp: string
+  level: LogLevel
+  namespace: string
+  message: string
+  data?: unknown
+  error?: unknown
+}
+
+export interface Transport {
+  log(entry: LogEntry): void
+}
