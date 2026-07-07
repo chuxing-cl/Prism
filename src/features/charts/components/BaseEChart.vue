214ebf1b722c01cae5595ea54e045df67c6a9c9d
初行 <3352514932@qq.com>
2026-07-07 16:27:42 +0800

    add src/features/charts/components/BaseEChart.vue

---
+++ src/features/charts/components/BaseEChart.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue"
+import { useResizeObserver } from "@/shared/composables"
+import { useECharts } from "@/features/charts/composables/useECharts"
+import type { EChartsOption } from "echarts"
+
+const props = defineProps<{
+  option: EChartsOption
+}>()
+
+const chartContainer = ref<HTMLElement | null>(null)
+const { init, initialized, setOption, resize } = useECharts(chartContainer)
+const { width, height } = useResizeObserver(chartContainer)
+
+onMounted(async () => {
+  await init()
+  if (initialized.value) {
+    setOption(props.option)
+  }
+})
+
+import { watch } from "vue"
+
+watch(() => props.option, (val) => {
+  if (initialized.value) setOption(val)
+}, { deep: true })
+
+watch([width, height], () => {
+  resize()
+})
+</script>
+
+<template>
+  <div ref="chartContainer" class="echart-container" />
+</template>
+
+<style scoped>
+.echart-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
