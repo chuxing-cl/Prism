f859ee1ecf624d1ac6018ea2f9836c628d7d8adb
初行 <3352514932@qq.com>
2026-07-07 16:28:06 +0800

    add src/router/index.ts

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "dashboard",
+      component: () => import("@/features/dashboard/views/DashboardView.vue"),
+    },
+  ],
+})
+
+export default router
