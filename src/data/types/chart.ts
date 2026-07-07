4b566b8ad99139db1b11f86a11d751e3ff0d7615
初行 <3352514932@qq.com>
2026-07-07 16:27:39 +0800

    add src/data/types/chart.ts

---
+++ src/data/types/chart.ts
@@ -0,0 +1,17 @@
+export interface ChartOption {
+  title?: string
+  animation?: boolean
+  theme?: "dark" | "light"
+}
+
+export interface ChartSeries {
+  name: string
+  type: "line" | "bar" | "pie"
+  data: unknown[]
+  color?: string
+}
+
+export interface ChartDimensions {
+  width?: number
+  height?: number
+}
