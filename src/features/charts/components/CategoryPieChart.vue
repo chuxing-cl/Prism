4d78ae9d2b3985f2267965a16b7b6d7306eae7d9
初行 <3352514932@qq.com>
2026-07-07 16:27:43 +0800

    add src/features/charts/components/CategoryPieChart.vue

---
+++ src/features/charts/components/CategoryPieChart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { useDashboardStore } from "@/features/dashboard/store"
+import BaseEChart from "./BaseEChart.vue"
+import { createCategoryPieOption } from "@/features/charts/config/category-pie"
+
+const store = useDashboardStore()
+const option = computed(() => createCategoryPieOption(store.categoryDistribution))
+</script>
+
+<template>
+  <BaseEChart :option="option" />
+</template>
