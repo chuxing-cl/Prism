63a8e635716761821e403d7f1976a5965eb60680
初行 <3352514932@qq.com>
2026-07-07 16:27:47 +0800

    add src/features/charts/components/RegionalBarChart.vue

---
+++ src/features/charts/components/RegionalBarChart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { useDashboardStore } from "@/features/dashboard/store"
+import BaseEChart from "./BaseEChart.vue"
+import { createRegionalBarOption } from "@/features/charts/config/regional-bar"
+
+const store = useDashboardStore()
+const option = computed(() => createRegionalBarOption(store.regionalStats))
+</script>
+
+<template>
+  <BaseEChart :option="option" />
+</template>
