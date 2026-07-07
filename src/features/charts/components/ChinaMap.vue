4f3e1b7f5b77e52b25408596c61353374a96a59f
初行 <3352514932@qq.com>
2026-07-07 16:58:05 +0800

    feat: 新增中国区域销售热力图 + 环境变量模板

---
+++ src/features/charts/components/ChinaMap.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { useDashboardStore } from "@/features/dashboard/store"
+import BaseEChart from "./BaseEChart.vue"
+import { registerChinaMap, createChinaMapOption } from "@/features/charts/config/china-map"
+
+// Register map immediately during setup, before child components mount
+registerChinaMap()
+
+const store = useDashboardStore()
+const option = computed(() => createChinaMapOption(store.regionalStats))
+</script>
+
+<template>
+  <BaseEChart :option="option" />
+</template>
